9e5d36b96d6a5d3861d776874b234f6ad28bc905
thunorsif@hotmail.com <thunorsif@hotmail.com@1bb8f794-9f0c-8f71-7f24-40670dc8f0a1>
2011-09-17 13:18:40 +0000

    Added widget_text.c/h.

---
+++ trunk/src/widget_text.c
@@ -0,0 +1,343 @@
+/*
+ * widget_text.c: 
+ * Gtkdialog - A small utility for fast and easy GUI building.
+ * Copyright (C) 2003-2007  László Pere <pipas@linux.pte.hu>
+ * Copyright (C) 2011 Thunor <thunorsif@hotmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/* Includes */
+#define _GNU_SOURCE
+#include <gtk/gtk.h>
+#include "config.h"
+#include "gtkdialog.h"
+#include "attributes.h"
+#include "automaton.h"
+#include "widgets.h"
+#include "signals.h"
+
+/* Defines */
+//#define DEBUG_CONTENT
+//#define DEBUG_TRANSITS
+
+/* Local function prototypes, located at file bottom */
+static void widget_text_input_by_command(variable *var, char *command);
+static void widget_text_input_by_file(variable *var, char *filename);
+static void widget_text_input_by_items(variable *var);
+
+/* Notes: */
+
+/***********************************************************************
+ * Clear                                                               *
+ ***********************************************************************/
+
+void widget_text_clear(variable *var)
+{
+	gchar            *var1;
+	gint              var2;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	gtk_label_set_text(GTK_LABEL(var->Widget), "");
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Create                                                              *
+ ***********************************************************************/
+GtkWidget *widget_text_create(
+	AttributeSet *Attr, tag_attr *attr, gint Type)
+{
+	GList            *element;
+	GtkWidget        *widget;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	/* Set a default label if unset */
+	attributeset_set_if_unset(Attr, ATTR_LABEL, "text");
+
+	widget = gtk_label_new(attributeset_get_first(&element, Attr, ATTR_LABEL));
+
+	/* Enable line wrapping by default */
+	gtk_label_set_line_wrap(GTK_LABEL(widget), TRUE);
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+
+	return widget;
+}
+
+/***********************************************************************
+ * Environment Variable All Construct                                  *
+ ***********************************************************************/
+
+gchar *widget_text_envvar_all_construct(variable *var)
+{
+	gchar            *string;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	/* This function should not be connected-up by default */
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+
+	return string;
+}
+
+/***********************************************************************
+ * Environment Variable Construct                                      *
+ ***********************************************************************/
+
+gchar *widget_text_envvar_construct(GtkWidget *widget)
+{
+	gchar            *string;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	string = g_strdup(gtk_label_get_text(GTK_LABEL(widget)));
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+
+	return string;
+}
+
+/***********************************************************************
+ * Fileselect                                                          *
+ ***********************************************************************/
+
+void widget_text_fileselect(
+	variable *var, const char *name, const char *value)
+{
+	gchar            *var1;
+	gint              var2;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	gtk_label_set_text(GTK_LABEL(var->Widget), value);
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Refresh                                                             *
+ ***********************************************************************/
+void widget_text_refresh(variable *var)
+{
+	GList            *element;
+	gchar            *act;
+	gint              initialised = FALSE;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	/* Get initialised state of widget */
+	if (g_object_get_data(G_OBJECT(var->Widget), "initialised") != NULL)
+		initialised = (gint)g_object_get_data(G_OBJECT(var->Widget), "initialised");
+
+	/* The <input> tag... */
+	act = attributeset_get_first(&element, var->Attributes, ATTR_INPUT);
+	while (act) {
+		if (input_is_shell_command(act))
+			widget_text_input_by_command(var, act + 8);
+		/* input file stock = "File:", input file = "File:/path/to/file" */
+		if (strncasecmp(act, "file:", 5) == 0 && strlen(act) > 5)
+			widget_text_input_by_file(var, act + 5);
+		act = attributeset_get_next(&element, var->Attributes, ATTR_INPUT);
+	}
+
+	/* The <item> tags... */
+	if (attributeset_is_avail(var->Attributes, ATTR_ITEM))
+		widget_text_input_by_items(var);
+
+	/* Initialise these only once at start-up */
+	if (!initialised) {
+		/* Apply directives */
+		if (attributeset_is_avail(var->Attributes, ATTR_DEFAULT))
+			fprintf(stderr, "%s(): <default> not implemented for this widget.\n",
+				__func__);
+		if (attributeset_is_avail(var->Attributes, ATTR_HEIGHT))
+			fprintf(stderr, "%s(): <height> not implemented for this widget.\n",
+				__func__);
+		if (attributeset_is_avail(var->Attributes, ATTR_WIDTH))
+			fprintf(stderr, "%s(): <width> not implemented for this widget.\n",
+				__func__);
+		if ((attributeset_cmp_left(var->Attributes, ATTR_SENSITIVE, "false")) ||
+			(attributeset_cmp_left(var->Attributes, ATTR_SENSITIVE, "disabled")) ||	/* Deprecated */
+			(attributeset_cmp_left(var->Attributes, ATTR_SENSITIVE, "no")) ||
+			(attributeset_cmp_left(var->Attributes, ATTR_SENSITIVE, "0")))
+			gtk_widget_set_sensitive(var->Widget, FALSE);
+
+		/* Connect signals */
+
+	}
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Removeselected                                                      *
+ ***********************************************************************/
+
+void widget_text_removeselected(variable *var)
+{
+	gchar            *var1;
+	gint              var2;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	fprintf(stderr, "%s(): Removeselected not implemented for this widget.\n",
+		__func__);
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Save                                                                *
+ ***********************************************************************/
+
+void widget_text_save(variable *var)
+{
+	gchar            *var1;
+	gint              var2;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	fprintf(stderr, "%s(): Save not implemented for this widget.\n", __func__);
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Input by Command                                                    *
+ ***********************************************************************/
+
+static void widget_text_input_by_command(variable *var, char *command)
+{
+	FILE             *infile;
+	GString          *text = g_string_sized_new(512);
+	gchar             line[512];
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+#ifdef DEBUG_CONTENT
+	fprintf(stderr, "%s(): command: '%s'\n", __func__, command);
+#endif
+
+	/* Opening pipe for reading... */
+	if (infile = widget_opencommand(command)) {
+		/* Read the file one line at a time */
+		while (fgets(line, 512, infile)) {
+			g_string_append(text, line);
+		}
+		gtk_label_set_text(GTK_LABEL(var->Widget), text->str);
+		g_string_free(text, TRUE);
+		/* Close the file */
+		pclose(infile);
+	} else {
+		fprintf(stderr, "%s(): Couldn't open '%s' for reading.\n", __func__,
+			command);
+	}
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Input by File                                                       *
+ ***********************************************************************/
+
+static void widget_text_input_by_file(variable *var, char *filename)
+{
+	FILE             *infile;
+	GString          *text = g_string_sized_new(512);
+	gchar             line[512];
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	if (infile = fopen(filename, "r")) {
+		/* Read the file one line at a time */
+		while (fgets(line, 512, infile)) {
+			g_string_append(text, line);
+		}
+		gtk_label_set_text(GTK_LABEL(var->Widget), text->str);
+		g_string_free(text, TRUE);
+		/* Close the file */
+		fclose(infile);
+	} else {
+		fprintf(stderr, "%s(): Couldn't open '%s' for reading.\n", __func__,
+			filename);
+	}
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
+
+/***********************************************************************
+ * Input by Items                                                      *
+ ***********************************************************************/
+
+static void widget_text_input_by_items(variable *var)
+{
+	gchar            *var1;
+	gint              var2;
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Entering.\n", __func__);
+#endif
+
+	fprintf(stderr, "%s(): <item> not implemented for this widget.\n", __func__);
+
+#ifdef DEBUG_TRANSITS
+	fprintf(stderr, "%s(): Exiting.\n", __func__);
+#endif
+}
