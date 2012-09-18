132e7d04cf663e5e74391739da0b550c9c45b618
thunorsif@hotmail.com <thunorsif@hotmail.com@1bb8f794-9f0c-8f71-7f24-40670dc8f0a1>
2012-09-18 21:32:41 +0000

    Added file-monitor functionality to the spinbutton widget. Added file-monitor functionality to the statusbar widget. Added file-monitor functionality to the table widget. Added file-monitor functionality to the terminal widget. Added file-monitor functionality to the text widget. Added file-monitor functionality to the timer widget. Added file-monitor functionality to the tree widget. Added file-monitor functionality to the window widget.

--- src/widget_table.c
+++ src/widget_table.c
@@ -346,8 +346,13 @@ void widget_table_refresh(variable *var)
 		if (input_is_shell_command(act))
 			widget_table_input_by_command(var, act + 8, TRUE);
 		/* input file stock = "File:", input file = "File:/path/to/file" */
-		if (strncasecmp(act, "file:", 5) == 0 && strlen(act) > 5)
+		if (strncasecmp(act, "file:", 5) == 0 && strlen(act) > 5) {
+			if (!initialised) {
+				/* Check for file-monitor and create if requested */
+				widget_file_monitor_try_create(var, act + 5);
+			}
 			widget_table_input_by_file(var, act + 5);
+		}
 		act = attributeset_get_next(&element, var->Attributes, ATTR_INPUT);
 	}
 
